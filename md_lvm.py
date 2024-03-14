83ec5b8a9a2aafd86c533b6bffd921ba2703ee27
Francesca <francesca.crucinio@warwick.ac.uk>
2024-03-14 17:15:09 +0000

    smc samlper

--- md_lvm.py
+++ md_lvm.py
@@ -26,11 +26,21 @@ def ll_toy_lvm(theta, x, data):
     return log_p
 
     
-def rwm_accept_toy_lvm(v, prop, theta_seq, gamma_seq, data):
-    seq_len = gamma_seq.size
+def rwm_accept_toy_lvm_varying_gamma(v, prop, theta_seq, gamma_seq, data):
+    n = theta_seq.size
     log_acceptance = 0.5*np.prod(1-gamma_seq)*np.sum((v - prop)**2, axis = 1)
-    for k in range(seq_len):
-        log_acceptance = log_acceptance + gamma_seq[k]*np.prod(1-gamma_seq[(k-1):])*(ll_toy_lvm(theta_seq[k], prop, data) - ll_toy_lvm(theta_seq[k], v, data))
+    for k in range(n):
+        log_acceptance = log_acceptance + gamma_seq[k]*np.prod(1-gamma_seq[(k+2):])*(ll_toy_lvm(theta_seq[k], prop, data) - ll_toy_lvm(theta_seq[k], v, data))
+    accepted = np.log(np.random.uniform(size = v.shape[0])) <= log_acceptance
+    output = ssp.view_2d_array(v)
+    output[accepted, :] = prop[accepted, :]
+    return output
+
+def rwm_accept_toy_lvm(v, prop, theta_seq, gamma, data):
+    n = theta_seq.size
+    log_acceptance = 0.5*((1-gamma)**n)*np.sum((v - prop)**2, axis = 1)
+    for k in range(n):
+        log_acceptance = log_acceptance + gamma*(1-gamma)**(n-k-2)*(ll_toy_lvm(theta_seq[k], prop, data) - ll_toy_lvm(theta_seq[k], v, data))
     accepted = np.log(np.random.uniform(size = v.shape[0])) <= log_acceptance
     output = ssp.view_2d_array(v)
     output[accepted, :] = prop[accepted, :]
@@ -44,18 +54,43 @@ def md_toy_lvm(y, gamma, Niter, N, th0, X0):
     x[0, :, :] = X0
     W = np.ones(N)/N
     for n in range(1, Niter):
-        theta[n] = theta[n-1] + gamma[n-1]*(np.sum(np.sum(x[n-1, :, :], axis = 1)*W)-D*theta[n-1])
+        theta[n] = theta[n-1] + gamma*(np.sum(np.sum(x[n-1, :, :], axis = 1)*W)-D*theta[n-1])
         if (n > 1):
             # resample
             ancestors = rs.resampling('stratified', W)
             x[n-1, :, :] = x[n-1, ancestors, :]
         # MCMC move
         prop = rwm_proposal(x[n-1, :, :], W)
-        x[n, :, :] = rwm_accept_toy_lvm(x[n-1, :, :], prop, theta[:n], gamma[:n], y)
+        x[n, :, :] = rwm_accept_toy_lvm(x[n-1, :, :], prop, theta[:n], gamma, y)
         # reweight
-        logW = ll_toy_lvm(theta[n], x[n, :, :], y) + 0.5*np.prod(1-gamma[:n])*np.sum(x[n, :, :]**2, axis = 1)
-        for k in range(n):
-            logW = logW - gamma[k]*np.prod(1-gamma[k:n])*ll_toy_lvm(theta[k], x[n, :, :], y)
+        logW = ll_toy_lvm(theta[n], x[n, :, :], y) + 0.5*((1-gamma)**(n-1))*np.sum(x[n, :, :]**2, axis = 1)
+        for k in range(n-1):
+            logW = logW - gamma*((1-gamma)**(n-k-2))*ll_toy_lvm(theta[k], x[n, :, :], y)
+        logW = gamma*logW
+        W = rs.exp_and_normalise(logW)
+    return theta, x, W
+
+
+def md_toy_lvm_varying_gamma(y, gamma, Niter, N, th0, X0):
+    D = X0.shape[1]
+    x = np.zeros((Niter, N, D))
+    theta = np.zeros(Niter)
+    theta[0] = th0
+    x[0, :, :] = X0
+    W = np.ones(N)/N
+    for n in range(1, Niter):
+        theta[n] = theta[n-1] + gamma[n]*(np.sum(np.sum(x[n-1, :, :], axis = 1)*W)-D*theta[n-1])
+        if (n > 1):
+            # resample
+            ancestors = rs.resampling('stratified', W)
+            x[n-1, :, :] = x[n-1, ancestors, :]
+        # MCMC move
+        prop = rwm_proposal(x[n-1, :, :], W)
+        x[n, :, :] = rwm_accept_toy_lvm_varying_gamma(x[n-1, :, :], prop, theta[:n], gamma[1:(n+1)], y)
+        # reweight
+        logW = ll_toy_lvm(theta[n], x[n, :, :], y) + 0.5*np.prod(1-gamma[1:n])*np.sum(x[n, :, :]**2, axis = 1)
+        for k in range(n-1):
+            logW = logW - gamma[k+1]*np.prod(1-gamma[(k+2):n])*ll_toy_lvm(theta[k], x[n, :, :], y)
         logW = gamma[n]*logW
         W = rs.exp_and_normalise(logW)
     return theta, x, W
